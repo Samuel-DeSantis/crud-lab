346798693d0c068054a34e364285eec9b7434845
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-02-02 22:37:52 -0700

    commit tree 5729be7848d1b08426f22afc8ece1fb40a5b456c

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,6 +1,22 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
  
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(
+    state = {
+        restaurants: [],
+        reviews: []
+    }, action) {
+
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const restaurant = {id: cuidFn(), text: action.text}
+            return {...state, restaurants: [...state, restaurant]}
+
+        // case 'DELETE_RESTAURANT':
+        //     return 1
+
+        default:
+            return state
+    }
 
 }
